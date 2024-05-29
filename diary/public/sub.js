3a161d01d391628873d0ed73611d5a123bbdffd9
jojayeon <jojayeon6152@gmail.com>
2024-05-29 15:50:06 +0900

    모듈로 json파일 가져오기

--- diary/public/sub.js
+++ diary/public/sub.js
@@ -1,16 +1,3 @@
-//nose.js 이다.
-
-
-
-
-
-//함수인거
-//module.exports = today;
-
-//불러올 곳에서
-//const today = require('./today.js')
-
-
-
-const obj = JSON.parse('./data.json');
+const obj = require('./data.json');
+module.exports = obj;
 console.log(obj);
