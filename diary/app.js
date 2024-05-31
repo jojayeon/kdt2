b97edf49d6120e58075742dbc395e75f0a4d62d8
jojayeon <jojayeon6152@gmail.com>
2024-05-31 14:39:57 +0900

    app.js에 readdir적용

--- diary/app.js
+++ diary/app.js
@@ -32,17 +32,24 @@ const server = http.createServer((req,res)=>{
     }
     //파일 위치 확인 해서 넣어주기
   //지금 부터는 서브 페이지 만들어지는 것 계속 들어가게
-  for(let i = 1; i < 366; i++) {
-    if(req.url === `/html/index${i}.html`){
-      fs.readFile(path.join(__dirname, `html/index${i}.html`), (err, data)=>{
-        if(err){
-          console.log("err~!~!~!~");
-        }
-        res.writeHead(200,{"content-type": "text/html; charset = utf-8"});
-        res.end(data);
-      });
+  let dir = path.join(__dirname, "html")
+  fs.readdir(dir, (err, file) => {
+    if(err){
+      console.log(err);
     }
-  }
+    let dirfile = file.length + 1
+    for(let i = 1; i < dirfile; i++) {
+      if(req.url === `/html/index${i}.html`){
+        fs.readFile(path.join(__dirname, `html/index${i}.html`), (err, data)=>{
+          if(err){
+            console.log("err~!~!~!~");
+          }
+          res.writeHead(200,{"content-type": "text/html; charset = utf-8"});
+          res.end(data);
+        });
+      }
+    }
+  });
   //css파일도 출력되게 만들기 - css는 2개 이니까 무식하게 만들자
   if(req.url === "/main.css"){
     fs.readFile(path.join(__dirname, "main.css"), (err, data)=>{
@@ -118,11 +125,18 @@ const server = http.createServer((req,res)=>{
   </body>
 </html>
         `
-        fs.writeFile(path.join(__dirname, `html/index${number}.html`),DATA, (err)=>{ //indexobj지우기
+        let dir = path.join(__dirname, "html")
+        fs.readdir(dir, (err, file) => {
           if(err){
             console.log(err);
           }
-        }); 
+          let dirfile = file.length + 1
+          fs.writeFile(path.join(__dirname, `html/index${dirfile}.html`),DATA, (err)=>{ //indexobj지우기
+            if(err){
+              console.log(err);
+            }
+          });//*표시 
+        });
       })
       fs.readFile(path.join(__dirname, "main.html"), (err, data)=>{
         if(err){
@@ -144,20 +158,3 @@ server.listen(3000, (err) =>{
   }
   console.log(`http://localhost:${PORT}`);
 });
-console.log(__dirname)
-let dir = '/Users/Administrator/Desktop/kdt2/diary/html';
-let dir1 = path.join(__dirname, "html")
-let dir2 = path.join(__dirname, "public")
-console.log(dir);
-console.log(dir1);
-console.log(dir2);
-
-let a = fs.readdir(dir2, (err, file) => {
-  if(err){
-    console.log(err);
-  }
-	console.log(file);
-  return file
-});
-
-console.log(a);
