24330c908d85ede03b993f85830314785203327b
jojayeon <jojayeon6152@gmail.com>
2024-05-31 14:39:12 +0900

    진짜 test.js끝

--- diary/test.js
+++ diary/test.js
@@ -8,15 +8,18 @@ const path = require("path");
 console.log(__dirname)
 let dir1 = '/Users/Administrator/Desktop/kdt2/diary/html';
 let dir2 = path.join(__dirname, "public")
-console.log(dir);
 console.log(dir1);
 console.log(dir2);
 
 
 //필요한 부분
 let dir = path.join(__dirname, "html")
-fs.readdir(dir, (err, file) => {
+let dirfilele = fs.readdir(dir, (err, file) => {
   if(err){
+    console.log(err);
   }
-	console.log(file.length);
+  return file.length + 1
 });
+console.log(dirfilele);
+
+console.log(dir);
